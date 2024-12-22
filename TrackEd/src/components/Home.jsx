dc955eb01e0ba0ccebee7817629acb03eed76132
Von Russel <monferovon@gmail.com>
2024-12-22 11:29:14 +0800

    up

--- TrackEd/src/components/Home.jsx
+++ TrackEd/src/components/Home.jsx
@@ -1,30 +1,17 @@
-import React from "react";
-import { Menu } from "lucide-react";
-import "./styles.css";
-import logo from "../assets/logo.png";
+import React, { useState } from 'react';
+import { Layout } from 'antd';
+import Logo from './Logo'
+
+const { Header, Sider } = Layout;
 
 function Home() {
     return (
-        <div className="home-content">
-        <header className="header">
-        <nav className="navbar">
-            <div className="navbar-left">
-                <Menu size={35} /> 
-                < a href="/home" className="logo">
-                    <img src={logo} alt="Logo" />
-                </a>
-            </div>
-        </nav>
-        </header>
-        <main className="home">
-        
-        
-        </main>
-        </div>
-
-        
+        <Layout>
+            <Sider className="sidebar">
+                <Logo />
+            </Sider>
+        </Layout>
     );
 }
-
 
 export default Home;
